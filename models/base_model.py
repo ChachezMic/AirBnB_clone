a5729f74863cb3fc8e200a6fcd276d77a75abefb
root <ChachezMic>
2024-03-26 12:36:56 -0700

    changes

--- models/base_model.py
+++ models/base_model.py
@@ -20,11 +20,12 @@ class BaseModel:
                     setattr(self, key, value)
                     if key in ('created_at', 'updated_at'):
                         datetime_format = "%Y-%m-%dT%H:%M:%S.%f"
-                        self.__dict__[key] =
-                        datetime.strptime(value, datetime_format)
+                        parsed_date = datetime.strptime(value, datetime_format)
+                        self.__dict__[key] = parsed_date
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
@@ -43,6 +44,7 @@ class BaseModel:
         returns a dictionary representation of the basemodel
         object
         """
+
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = self.__class__.__name__
         for key, value in my_dict.items():
