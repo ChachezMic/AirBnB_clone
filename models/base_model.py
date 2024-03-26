23632af09844415e255b67785043fcf723213097
root <ChachezMic>
2024-03-26 08:02:09 -0700

    kila

--- models/base_model.py
+++ models/base_model.py
@@ -16,18 +16,19 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = self.created_at
+
     def __str__(self):
         """
         string representation of the basemodel object
         """
         return f"[BaseModel] ({self.id}) {self.__dict__}"
-    
+
     def save(self):
         """
         updates the the public instace attribute update_at with the current utc
         """
         self.updated_at = datetime.utcnow()
-    
+
     def to_dict(self):
         """
         returns a dictionary representation of the basemodel
