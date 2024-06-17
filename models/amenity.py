c73bff83ed5bdd81e0d152dd66b98f0cdeae85aa
ChachezMic <chachezmic@gmail.com>
2024-06-17 15:44:11 -0700

    what

--- models/amenity.py
+++ models/amenity.py
@@ -1,10 +1,6 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """class module for hbnb project for the amenity"""
 from models.base_model import BaseModel
 
-
 class Amenity(BaseModel):
-    """
-    this class contains name attribute
-    """
     name = ""
